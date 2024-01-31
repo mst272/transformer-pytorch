d4c027fd65903667f7c8439982007bdfdaa4504c
wangzhihong <sdwzh272@163.com>
2024-02-01 00:02:07 +0800

    Update

--- MOE/moe_make.py
+++ MOE/moe_make.py
@@ -32,11 +32,29 @@ class TopKRouter(nn.Module):
         zeros = torch.full_like(logits, float('-inf'))
 
 
+class NoisyTopKRouter(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super().__init__()
+        self.top_k = top_k
+        self.topkrouter_linear = nn.Linear(n_embed, num_experts)
+        self.noisy_linear = nn.Linear(n_embed, num_experts)
 
+    def forward(self, x):
+        # x is the output tensor from multi-head self attention block
+        logits = self.topkrouter_linear(x)
 
+        # Noise logits
+        noise_logits = self.noisy_linear(x)
 
+        # Adding scaled unit gaussian noise to the logits
+        noise = torch.rand_like(logits)*F.softplus(noise_logits)
+        noise_added_logits = logits + noise
 
-
+        top_k_logits, indices = noise_added_logits.topk(self.top_k, dim=-1)
+        zeros = torch.full_like(noise_logits, float('-inf'))
+        sparse_logits = zeros.scatter(-1, indices, top_k_logits)
+        router_output = F.softmax(sparse_logits, dim=-1)
+        return router_output, indices
 
 
 
