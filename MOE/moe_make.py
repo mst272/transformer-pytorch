78105a9e0266dba2a80cb2d5c6f3c6305e1bec68
wangzhihong <wangzhihong@cmss.chinamobile.com>
2024-02-01 18:27:04 +0800

    update

--- MOE/moe_make.py
+++ MOE/moe_make.py
@@ -6,7 +6,7 @@ torch.manual_seed(42)
 
 
 class Expert(nn.Module):
-    def __init__(self, n_embed, dropout):
+    def __init__(self, n_embed, dropout=0.1):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(n_embed, 4 * n_embed),
@@ -47,7 +47,7 @@ class NoisyTopKRouter(nn.Module):
         noise_logits = self.noisy_linear(x)
 
         # Adding scaled unit gaussian noise to the logits
-        noise = torch.rand_like(logits)*F.softplus(noise_logits)
+        noise = torch.rand_like(logits) * F.softplus(noise_logits)
         noise_added_logits = logits + noise
 
         top_k_logits, indices = noise_added_logits.topk(self.top_k, dim=-1)
@@ -57,7 +57,28 @@ class NoisyTopKRouter(nn.Module):
         return router_output, indices
 
 
+class SparseMOE(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super().__init__()
+        self.router = NoisyTopKRouter(n_embed, num_experts, top_k)
+        self.experts = nn.ModuleList([Expert(n_embed) for _ in range(num_experts)])
+        self.top_k = top_k
+
+    def forward(self, x):
+        gating_output, indices = self.router(x)
+        print(f"gating output:{gating_output}", '\n' f"indices:{indices}")
+        final_output = torch.zeros_like(x)
+        print(f"final_output:\n {final_output}")
+
+        flat_x = x.view(-1, x.size(-1))
+        print(f"flat_x:{flat_x} \n flat_x size:{flat_x.size()}")
 
 
-
-
+if __name__ == '__main__':
+    num_experts = 8
+    top_k = 2
+    n_embd = 6
+    dropout = 0.1
+    mh_output = torch.randn(3, 4, 6)
+    smoe = SparseMOE(n_embd, num_experts, top_k)
+    smoe(mh_output)
